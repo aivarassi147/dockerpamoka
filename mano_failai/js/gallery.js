cd7ca4e2d078ae05e113f520c59fef0fdd5b8f64
aivarasimkus9@gmail.com <aivarasimkus9@gmail.com>
2023-08-11 15:09:57 +0300

    patvarkytas

--- mano_failai/js/gallery.js
+++ mano_failai/js/gallery.js
@@ -1,32 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const galleryData = [
+    const duomenysKuriuosSaugojamIStorage = [
         {
             title: 'Darbas 1',
-            img_url: 'pics/img.jpg',
+            img_url: 'https://picsum.photos/200/300?random=1',
             tags: ['js', 'html', 'css', 'python'],
         },
         {
             title: 'Darbas 2',
-            img_url: 'pics/img.jpg',
+            img_url: 'https://picsum.photos/200/300?random=2',
             tags: ['js', 'python'],
         },
         {
             title: 'Darbas 3',
-            img_url: 'pics/img.jpg',
+            img_url: 'https://picsum.photos/200/300?random=3',
             tags: ['python'],
         }
     ];
 
+    if (localStorage.getItem('gallery') === null) {
+        localStorage.setItem('gallery', JSON.stringify(duomenysKuriuosSaugojamIStorage));
+    }
+    galleryData = JSON.parse(localStorage.getItem('gallery'));
+
     let galleryDOMelement = document.querySelector('#work_gallery');
 
-    for (let DataofGalery of galleryData) {
+    for (let galleryElementData of galleryData) {
         const div = document.createElement('div');
         const img = document.createElement('img');
         const span = document.createElement('span');
 
-        img.src = DataofGalery.img_url;
-        img.alt = DataofGalery.title;
-        span.innerText = DataofGalery.title;
+        img.src = galleryElementData.img_url;
+        img.alt = galleryElementData.title;
+        span.innerText = galleryElementData.title;
 
         div.append(img);
         div.append(span);
--- mano_failai/js/storage.js
+++ mano_failai/js/storage.js
@@ -1,18 +1,19 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
 
-        const duomenysDOM = document.getElementById('duomenys');
-        const atsakymasDOM = document.getElementById('atsakymas');
-        const mygtukasDOM = document.getElementById('mygtukas');
+    const duomenysDOM = document.getElementById('duomenys');
+    const atsakymasDOM = document.getElementById('atsakymas');
+    const mygtukasDOM = document.getElementById('mygtukas');
 
 
-        mygtukasDOM.addEventListener('click', () => {
-            const laukelioDuomenys = duomenysDOM.value;
-            localStorage.setItem('lankytojoVardas', laukelioDuomenys);
-        });
+    mygtukasDOM.addEventListener('click', () => {
+        const laukelioDuomenys = duomenysDOM.value;
+        localStorage.setItem('lankytojoVardas', laukelioDuomenys);
+    });
 
-        const lankytojoVardas = localStorage.getItem('lankytojoVardas');
-        if (lankytojoVardas){
-            atsakymasDOM.textContent = lankytojoVardas;
-        }
+
+    const lankytojoVardas = localStorage.getItem('lankytojoVardas');
+    if (lankytojoVardas) {
+        atsakymasDOM.textContent = lankytojoVardas;
+    }
 
 });
